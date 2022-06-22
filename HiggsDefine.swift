0d5d628f736f0bbbc1be48baba2d307c76bc9967
qushiyao <a861072419a@qq.com>
2022-06-22 11:49:46 +0800

    de3fe

---
+++ HiggsDefine.swift
@@ -0,0 +1,15 @@
+//
+//  HiggsDefine.swift
+//  slock
+//
+//  Created by qushiyao on 2021/6/21.
+//
+
+import Foundation
+import UIKit
+let RGBMainColor = UIColor(red: 84/255.0, green: 93/255.0, blue: 255/255.0, alpha: 1)
+func RGB(r:CGFloat,_ g:CGFloat,_ b:CGFloat, alpha:CGFloat) ->UIColor
+{
+return UIColor(red: (r)/255.0, green: (g)/255.0, blue: (b)/255.0, alpha: alpha)
+}
+
